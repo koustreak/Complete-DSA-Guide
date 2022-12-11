760480913ecf6f8aadd73292bf23043d760ce55c
koushik <dot.py@yahoo.com>
2022-12-11 17:23:31 +0530

    tree

---
+++ Tree/tree_traversal.py
@@ -0,0 +1,59 @@
+# In order traversal
+
+# LDR and RDL
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def inorder_ldr(root):
+    if not root:
+        return None
+
+    inorder_ldr(root.left)
+    print(root.data,sep='-->',end='-->')
+    inorder_ldr(root.right)
+
+def inorder_rdl(root):
+    if not root:
+        return
+
+    inorder_rdl(root.right)
+    print(root.data,sep='-->',end='-->')
+    inorder_rdl(root.left)
+
+def preorder_dlr(root):
+    if not root:
+        return
+
+    print(root.data,sep='-->',end='-->')
+    preorder_dlr(root.left)
+    preorder_dlr(root.right)
+
+
+def preorder_drl(root):
+    if not root:
+        return
+
+    print(root.data, sep='-->', end='-->')
+    preorder_dlr(root.right)
+    preorder_dlr(root.left)
+
+
+def postorder_lrd(root):
+    if not root:
+        return
+
+    preorder_dlr(root.left)
+    preorder_dlr(root.right)
+    print(root.data, sep='-->', end='-->')
+
+def postorder_rld(root):
+    if not root:
+        return
+
+    preorder_dlr(root.right)
+    preorder_dlr(root.left)
+    print(root.data, sep='-->', end='-->')
